32e5a0b3d08fef3b9a3f7cdc872bea6bdb2584a1
efrain <efsolora@gmail.com>
2022-04-28 11:45:39 -0500

    Curso de JavaScript Básico

---
+++ index.js
@@ -0,0 +1,8 @@
+let cadenaDeTexto = "hola, mi nombre es efrain";
+
+function dosVariables() {
+  let cadenaDeTexto = "hola, mi apellido es solorzano";
+  console.log(cadenaDeTexto);
+}
+dosVariables();
+console.log(cadenaDeTexto);
